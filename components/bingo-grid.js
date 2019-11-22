fb0e0f0e74d3333245a54cdd4dd945b1957dba0f
Robin Duda <robin.duda@idainfront.se>
2019-11-22 03:21:00 +0100

    improved print style and browser support

--- components/bingo-grid.js
+++ components/bingo-grid.js
@@ -1,11 +1,19 @@
 class BingoGrid extends HTMLElement {
     connectedCallback() {
+        this.align()
+    }
+
+    align() {
         this.style['flex-basis'] = `${100 / this.getAttribute('grid-columns')}%`;
         this.style['width'] = `${this.getAttribute('grid-columns') * 198}px`;
     }
 
-    update(tiles) {
-        for (let text of tiles) {
+    update(layout) {
+        this.setAttribute('grid-columns', layout.columns);
+        this.align();
+        console.log(layout);
+
+        for (let text of layout.tiles) {
             let tile = document.createElement('bingo-tile');
             tile.setAttribute('text', text);
             this.appendChild(tile);
